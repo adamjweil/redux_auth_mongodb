714687a1f5dec69c0649dfef030a132b2a3dbe16
Adam Weil <adamweil@Adams-MacBook-Pro.local>
2020-12-10 14:39:46 -0500

    listing out experience and edu on profile

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -5,6 +5,8 @@ import { Link } from 'react-router-dom';
 import Spinner from '../layout/Spinner';
 import { getCurrentProfile } from '../../actions/profile';
 import DashboardActions from './DashboardActions';
+import Experience from './Experience';
+import Education from './Education';
 
 const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, loading } }) => {
     useEffect(() => {
@@ -19,6 +21,8 @@ const Dashboard = ({ getCurrentProfile, auth: { user }, profile: { profile, load
         { profile !== null ? (
         <Fragment>
             <DashboardActions />
+            <Experience experience={profile.experience} />
+            <Education education={profile.education} />
         </Fragment>
          ) : (
          <Fragment>

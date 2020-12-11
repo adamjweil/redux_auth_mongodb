2c956dd1e3366ee61861e40e9bd108b050b5034c
Adam Weil <adamweil@Adams-MBP.lan1>
2020-12-11 11:59:09 -0500

    added social icons to the profile page; this couldve been been done using aa reusable component, not sure why I did it this way

---
+++ client/src/components/profile/social/InstagramIcon.js
@@ -0,0 +1,13 @@
+import React, { Fragment } from 'react'
+import PropTypes from 'prop-types'
+import instagram from './img/instagram.png';
+
+export default () => (
+    <Fragment>
+        <img 
+            src={instagram}
+            style={{width: '50px', height: '50px', marginLeft: '15px', display: 'in-line' }}
+            alt='Loading....' 
+        />
+    </Fragment>
+);
